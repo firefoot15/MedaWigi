d597731f20ed16faffb93f0e74a9b9128bf22c68
sophiathach <sophia.thach@gmail.com>
2015-10-20 09:59:56 -0400

    Create index.php
    index.php contains login

---
+++ index.php
@@ -0,0 +1,27 @@
+<html>
+	<head> 
+		<title> MedaWiGi</title>
+	</head>
+	<body>
+		<center>
+		<h1> MedaWiGi</h1>
+		<img src="http://tomshattuck.typepad.com/.a/6a01538f4b955c970b017ee8bf71a8970d-pi" 
+		style="width: 300px; height 500px;">
+
+		<br/>
+
+
+		<form action="checklogin.php" method="POST">
+			Enter Username:<input type="text" name="username" required="required" /> <br/>
+			Enter Password: <input type="password" name="password" required="required" /> <br/>
+
+			<a href="register.php">New User?</a> <br/>
+			<a href="forgotpass.php"> Forgot Password?</a> <br/>
+
+			<input type="submit" value="Login" />
+		</form>
+		
+		</center>
+
+</body>
+</html>
