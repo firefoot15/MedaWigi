51e78893d459e78f164130e7b4a5eefa62702fd3
sophiathach <sophia.thach@gmail.com>
2015-11-15 10:48:25 -0500

    javascript file for menu onclick

---
+++ mediwigi.js
@@ -0,0 +1,11 @@
+<script>
+function menu() {
+  if (document.getElementById("menu-list").style.visibility == "hidden") {
+    document.getElementById("menu-list").style.visibility = "visible";
+    
+  } else {
+    document.getElementById("menu-list").style.visibility = "hidden";
+    
+  }
+}
+</script>
