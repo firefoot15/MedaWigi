ec06f247cde4d5f1737a17c038f9a58a672a6aac
Curtis Sunley Jr <casunleyjr@gmail.com>
2015-11-15 12:20:50 -0500

    Framework for adding registered people

--- addperson.php
+++ addperson.php
@@ -50,9 +50,6 @@ include 'connect.php';
 session_start();
 $switchBool = false;
 
-//Global variable to count number of people being added
-$personNumber;
-
 //Check to see if user is the same
 if($_SESSION['user']){}
 		else{
@@ -62,14 +59,85 @@ if($_SESSION['user']){}
 //Store username that's logged in        
 $user = $_SESSION['user'];
     
-//SQL Operations
-//Adding the person onto the account that is logged in
+    /*SQL Operations*/
+
     
     //If adding on a registered user
     if(isset($_POST['submitRegisteredUserButton'])){
+        
+        /*Grab necessary information from queries and store it*/
+        
+        //Store posted email address
         $email = mysql_real_escape_string($_POST['newRegisteredUserText']);
-        $queryOne = mysql_query("Select * from accounts WHERE username = '$user'");
-        $queryTwo = mysql_query("Select * from accounts WHERE email = '$email'");
+        
+        //Store accountID of registered user that is being added onto account as a person
+        $registeredUserQuery = mysql_query("Select * from accounts WHERE email = '$email' limit 1");
+        $registeredUserQueryRow = mysql_fetch_array($registeredUserQuery);
+        $registeredUserAccountID = $registeredUserQueryRow[0];
+        
+        //Store accountID of user that is logged in and adding on a new registered user as a person
+        $accountQuery = mysql_query("Select * from accounts WHERE username = '$user' limit 1");
+        $accountQueryRow = mysql_fetch_array($accountQuery);
+        $accountID = accountQueryRow[0];
+        
+        //Store personID of user that is logged in and adding on a new registered user as a person
+        $personQuery = mysql_query("Select * from persons WHERE accountID = '$accountID'");
+        $personQueryRow = mysql_fetch_array($personQuery);
+        $personID = $personQueryRow[0];
+        
+        //Store personID of registered user that is being added onto account as a person
+        $newRegisteredPersonQuery = mysql_query("Select * from persons WHERE accountID = '$registeredUserAccountID'");
+        $newRegisteredPersonQueryRow = mysql_fetch_array($newRegisteredPersonQuery);
+        $newRegisteredPersonID = $newRegisteredPersonQueryRow[0];
+        
+        //Query for correct row in mappings
+        $mappingsQuery = mysql_query("SELECT * from mappings where accountID = '$accountID'");
+        $row = mysql_fetch_array($mappingsQuery);
+        
+        for($i = 1; $i < 10 && $switchBool == false; $i++)
+            {
+                $colName = $i.'_personID';
+            
+                    //While that SQL cell is empty/set to 0
+                    if($row[$colName] == 0)
+                    {
+                        $switchBool = true;
+                        switch($i)
+                        {
+                            case 1:
+                                mysql_query("UPDATE mappings SET 1_personID = '$newRegisteredPersonID' WHERE accountID ='$accountID'");
+                                break;
+                            case 2:
+                                mysql_query("UPDATE mappings SET 2_personID = '$newRegisteredPersonID' WHERE accountID ='$accountID'");
+                                break;
+                            case 3:
+                                mysql_query("UPDATE mappings SET 3_personID = '$newRegisteredPersonID' WHERE accountID ='$accountID'");
+                                break;
+                            case 4:
+                                mysql_query("UPDATE mappings SET 4_personID = '$newRegisteredPersonID' WHERE accountID ='$accountID'");
+                                break;
+                            case 5:
+                                mysql_query("UPDATE mappings SET 5_personID = '$newRegisteredPersonID' WHERE accountID ='$accountID'");
+                                break;
+                            case 6:
+                                mysql_query("UPDATE mappings SET 6_personID = '$newRegisteredPersonID' WHERE accountID ='$accountID'");
+                                break;
+                            case 7:
+                                mysql_query("UPDATE mappings SET 7_personID = '$newRegisteredPersonID' WHERE accountID ='$accountID'");
+                                break;
+                            case 8:
+                                mysql_query("UPDATE mappings SET 8_personID = '$newRegisteredPersonID' WHERE accountID ='$accountID'");
+                                break;
+                            case 9:
+                                mysql_query("UPDATE mappings SET 9_personID = '$newRegisteredPersonID' WHERE accountID ='$accountID'");
+                                break;    
+                        }
+                        
+                    }
+                }
+        //Alert successful add and move to myportal
+//            Print '<script>alert("The new person was successfully added!");</script>';
+//            Print '<script>window.location.assign("myportal.php");</script>';
     }
     
     //If adding on a new person
@@ -79,28 +147,23 @@ $user = $_SESSION['user'];
         $lastName = mysql_real_escape_string($_POST['lastNameText']);
         
         //Grab and store accountID
-        $query = mysql_query("Select accountID from accounts WHERE username = '$user' limit 1");
-        $accountID = mysql_result($query, 0);
+        $accountQuery = mysql_query("Select accountID from accounts WHERE username = '$user' limit 1");
+        $accountID = mysql_result($accountQuery, 0);
         
         //Take row in mappings table for user's accountID, store in row var
-        $queryTwo = mysql_query("SELECT * from mappings where accountID = '$accountID'");
-        $row =  mysql_fetch_array($queryTwo);
-        
-        
+        $mappingsQuery = mysql_query("SELECT * from mappings where accountID = '$accountID'");
+        $row =  mysql_fetch_array($mappingsQuery);    
+            
             //Query to insert into persons table
             mysql_query("INSERT INTO persons (firstName, lastName, gender, race, nickname) VALUES ('$firstName','$lastName', 'Unspecified', 'Unspecified','$nickname')");
             //Auto incremented value stored in variable
             $personID = mysql_insert_id();
-//            echo "<script type='text/javascript'>alert('$personID');</script>";
-            
-            
 
                 //Switch statement that inserts into specified column....hopefully
                 for($i = 1; $i < 10 && $switchBool == false; $i++)
                 {
-                    //$row[$colName] == 0
                     $colName = $i.'_personID';
-                    //What needs to be added here?
+                    //While that SQL cell is empty/set to 0
                     if($row[$colName] == 0)
                     {
                         $switchBool = true;
