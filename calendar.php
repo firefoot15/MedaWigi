421e204b3f1048b14f15795edfaa4f97629f318b
lkwhitesell <lkwhites@uncg.edu>
2015-11-15 12:22:29 -0500

    Creating events in calendar, but not fully functional yet.

--- calendar.php
+++ calendar.php
@@ -1,60 +1,100 @@
-<?php
-include 'connect.php';
-session_start();
-
-/* Verify Username */
-if($_SESSION['user']){}
-else {
-    header("location:index.php"); }
-
-$user = $_SESSION['user'];
-
-/* Selct accountID of given username */
-$query = mysql_query("Select accountID from accounts WHERE username = '$user' limit 1");
-$accountID = mysql_result($query,0);
-
-/* Select existing journal entries for accountID */
-$query = mysql_query("Select * from events WHERE accountID='$accountID' ORDER BY journalDate ASC, journalTime ASC");
-$exists = mysql_num_rows($query);
-
-if($exists>0) {
-    while ($row = mysql_fetch_assoc($query)) {
-   
-    }
-}
-
-?>
-
 <html>
-    <head>
-        <title>Calendar</title>
-        <script src="http://code.jquery.com/jquery-1.11.3.min.js"></script>
-        <!-- Latest compiled and minified CSS -->
-        <link rel="stylesheet"  href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" integrity="sha512-dTfge/zgoMYpP7QbHy4gWMEGsbsdZeCXz7irItjcC3sPUFtf0kuFbDz/ixG7ArTxmDjLXDmezHubeNikyKGVyQ==" crossorigin="anonymous">
-        <script src="zabuto_calendar.js"></script>
-        <link rel="stylesheet" type="text/css" href="calendarstyle.css">
+
+<head>
+    <title>Calendar</title>
+    <script src="http://code.jquery.com/jquery-1.11.3.min.js"></script>
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" integrity="sha512-dTfge/zgoMYpP7QbHy4gWMEGsbsdZeCXz7irItjcC3sPUFtf0kuFbDz/ixG7ArTxmDjLXDmezHubeNikyKGVyQ==" crossorigin="anonymous">
+    <script src="zabuto_calendar.js"></script>
+    <link rel="stylesheet" type="text/css" href="calendarstyle.css">
+
+    <div id="my-calendar"></div>
+
+
+</head>
+
+<body>
+
+
+
+    <?php
+
+    include 'connect.php';
+
+    session_start();
+
+    /* Verify Username */
+    if($_SESSION['user']){}
+    else {
+        header("location:index.php"); }
+
+    $user = $_SESSION['user'];
+
+    /* Selct accountID of given username */
+    $query = mysql_query("Select accountID from accounts WHERE username = '$user' limit 1");
+    $accountID = mysql_result($query,0);
+
+    // Using accountID, populate array with persons 
+    $query = mysql_query("Select * from mappings WHERE accountID = '$accountID'");
+    $row = mysql_fetch_array($query);
+    $personIDArray = array();
+    for($i=0; $i<10; $i++) {
+        $colName = $i.'_personID';
+        $personID = abs($row[$colName]);
+        if($personID != 0) {
+            array_push($personIDArray, $personID);
+        }
+    }
+
+
         
 
-
-    </head>
+    for($i=0; $i<count($personIDArray); $i++) {
+        $personID = $personIDArray[$i];
+        $query = mysql_query("Select * from events WHERE personID='$personID'");
     
-    <body>
+        while($row = mysql_fetch_array($query)) {
+            $date = $row['eventDate'];  
         
-        <div id="my-calendar"></div>
-
-        <script type="application/javascript">
-            $(document).ready(function () {
-                $("#my-calendar").zabuto_calendar({
-                    language: "en",
-                    cell_border: true,
-                    today: true,
-                    
-                     
-                });
-            });
+    
+            echo '<script language="javascript">
+            var js_var = "<?php echo $date;?>";
             
-        </script>
+            var eventData = {
+            date: js_var,
+            badge: true,
+            title: "Event",
+            body: "",
+            footer: "",
+            classname: ""
+            };
+            </script>';
+        
+            echo '<script>alert(eventData); </script>';
+        }
+    }
     
-    </body>
+        ?>
     
+    
+    
+    <script>
+        $(document).ready(function () {
+            $("#my-calendar").zabuto_calendar({
+                language: "en",
+                cell_border: true,
+                today: true,
+                data: eventData
+            });
+    
+        });
+        
+    </script>
+
+
+
+
+
+</body>
+
 </html>
