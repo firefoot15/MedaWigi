6b9572621d1fbb9b8b72e639150c84aa5e0cc050
Sophia Thach <sophia.thach@gmail.com>
2015-11-16 13:09:43 -0500

    styling changes for logo

--- connect.php
+++ connect.php
@@ -4,11 +4,11 @@ File designed to coonnect to database
 Type include 'connect.php': at the start of a php file to get it going!
 Modualization at its finest
 */
-$localSQL = mysql_connect("localhost", "root", "") or die(mysql_error()); 
-	mysql_select_db("medawigi") or die("Cannot connect to database.");
+//$localSQL = mysql_connect("localhost", "root", "") or die(mysql_error()); 
+	//mysql_select_db("medawigi") or die("Cannot connect to database.");
 
-//$serverSQL = mysql_connect("localhost:33333", "firefoot15", "thanksforallthefish") or die(mysql_error());
-// mysql_select_db("medawigi") or die("Cannot connect to database.");
+$serverSQL = mysql_connect("localhost:33333", "firefoot15", "thanksforallthefish") or die(mysql_error());
+mysql_select_db("medawigi") or die("Cannot connect to database.");
 
 
 ?>
