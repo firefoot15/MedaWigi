100d54f1f8a708b123f367c0a5ab4f5a61b336ba
Curtis <casunley@uncg.edu>
2015-11-17 10:18:31 -0500

    Framework for forgot pass, formatting for addperson

--- forgotpass.php
+++ forgotpass.php
@@ -1,78 +1,120 @@
+<?php
+include 'connect.php';
+session_start();
+if(isset($_POST['submitEmailButton'])){
+    $email = mysql_real_escape_string($_POST['email']);
+    $emailDoesNotExistBool = false;
+    
+    //Grab the secret question based off of email
+    $secretQuestQuery = mysql_query("SELECT secretQuest from accounts WHERE email ='$email' limit 1");
+    
+    if(mysql_numrows($secretQuestQuery) !=0){
+        $emailDoesNotExistBool = false;
+        $secretQuest = mysql_result($secretQuestQuery,0);
+    }
+    else{
+        $emailDoesNotExistBool = true;
+    }
+}
+
+
+?>
+
 <html>
 
 <head>
     <title>Forgot Your Password?</title>
-<link rel="stylesheet" type="text/css" href="style.css">
-<link href='https://fonts.googleapis.com/css?family=Oswald' rel='stylesheet' type='text/css'>
-<script src="medawigi.js"></script>
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <link href='https://fonts.googleapis.com/css?family=Oswald' rel='stylesheet' type='text/css'>
+    <script src="medawigi.js"></script>
 </head>
 <div class="top">
-  <div id="logo">
-    <img />
-    
-  </div>
-  <div class="sammich">
-    <img onclick="menu()" class="sammich" src="https://cdn2.iconfinder.com/data/icons/menu-elements/154/round-border-menu-bar-128.png" />
+    <div id="logo">
+        <img />
 
-
-
-    <div class="menu">
-      <ul id="menu-list">
-        <br>
-        <li id="home"><a href="personhome.php">Home</a></li>
-        <br>
-        <li id="insurance_contact"><a href="insurance.php">Insurance</a></li>
-        <br>
-        <li id="calendar"><a href="calendar.php">Calendar</a></li>
-        <br>
-        <li id="journal"><a href="journal.php">Journal</a></li>
-        <br>
-        <li id="medications"><a href="medication.php">Medications</a></li>
-        <br>
-        <li id="allergies"><a href="allergies.php">Allergies</a></li>
-        <br>
-	<li id="immunizations"><a href="immunizations.php">Immunizations</a></li>
-	<br>
-	<li id="contacts"><a href="contacts.php">Contacts</a></li>
-	<br>
-	<li id="conditions"><a href="conditions.php">Conditions</a></li>
-	<br>
-        <li id="contact"><a href="contact.html">Contact us</a></li>
-        <br>
-        <li id="editprofile"><a href="editprofile.php">Edit Profile</a></li>
-        <br>
-        <li id="switch_profile"><a href="#">Switch Profile</a></li>
-        <br>
-        <li id="logout"><a href="logout.php">Logout</a></li>
-        <br>
-      </ul>
     </div>
-  </div>
+    <div class="sammich">
+        <img onclick="menu()" class="sammich" src="https://cdn2.iconfinder.com/data/icons/menu-elements/154/round-border-menu-bar-128.png" />
+
+
+
+        <div class="menu">
+            <ul id="menu-list">
+                <br>
+                <li id="home"><a href="personhome.php">Home</a></li>
+                <br>
+                <li id="insurance_contact"><a href="insurance.php">Insurance</a></li>
+                <br>
+                <li id="calendar"><a href="calendar.php">Calendar</a></li>
+                <br>
+                <li id="journal"><a href="journal.php">Journal</a></li>
+                <br>
+                <li id="medications"><a href="medication.php">Medications</a></li>
+                <br>
+                <li id="allergies"><a href="allergies.php">Allergies</a></li>
+                <br>
+                <li id="immunizations"><a href="immunizations.php">Immunizations</a></li>
+                <br>
+                <li id="contacts"><a href="contacts.php">Contacts</a></li>
+                <br>
+                <li id="conditions"><a href="conditions.php">Conditions</a></li>
+                <br>
+                <li id="contact"><a href="contact.html">Contact us</a></li>
+                <br>
+                <li id="editprofile"><a href="editprofile.php">Edit Profile</a></li>
+                <br>
+                <li id="switch_profile"><a href="#">Switch Profile</a></li>
+                <br>
+                <li id="logout"><a href="logout.php">Logout</a></li>
+                <br>
+            </ul>
+        </div>
+    </div>
 
 </div>
 
 
 <body>
-    <h2>Forgot Your Password?</h2>
+    <h1>Forgot Your Password?</h1>
     <br/>
     <form id="emailcheck" action="forgotpass.php" method="POST">
         Email:
         <input type="text" id="email" name="email" required="required" />
-        <input type="submit" name="submit" />
-        <input type="button" name="cancel" value="Cancel" onclick="document.location.href='index.php'" />
+        <input type="submit" name="submitEmailButton" value="Submit" onclick="showSecretCheckForm();" class="basic_button" />
+        <input type="reset" name="cancel" value="Cancel" onclick="document.location.href='index.php'" class="basic_button" />
     </form>
     <br/>
     <br/>
 
-    <form id="secretcheck" action="forgotpass.php" style="display:none;" method="POST">
-        What is your stripper name?:
-        <input type="text" id="secretquestionform" name="secretquestionform" required="required" />
-        <input type="submit" name="secretsubmit" />
-        <input type="button" name="secretcancel" value="Cancel" />
+    <form action="forgotpass.php" id="secretcheck" action="forgotpass.php" style="display:none;" method="POST">
+        <?php
+        echo $secretQuest;
+        ?>
+        <input type="text" id="secretQuestionForm" name="secretQuestionForm" required="required" />
+        <input type="submit" name="secretQuestionSubmitButton" value="Submit" class="basic_button"/>
+        <input type="reset" name="secretQuestionCancelButton" value="Cancel" class="basic_button" onclick="document.location.href='index.php'" />
     </form>
 </body>
 
 </html>
+
+<script>
+    function showSecretCheckForm(){
+        document.getElementById('secretCheck').style.display = "block";
+    }
+    
+    function hideSecretCheckForm(){
+        document.getElementById('secretCheck').style.display = "block";
+    }
+    
+    function showEmailCheckForm(){
+        document.getElementById('emailCheck').show.display = "block";
+}
+    
+    function hideEmailCheckForm(){
+        document.getElementById('emailcheck').show.display = "none";
+    }
+</script>
 
 <?php
 //if($_SERVER["REQUEST_METHOD"] == "POST"){
