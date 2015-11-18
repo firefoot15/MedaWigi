befb0961f1e60948d7782546d489c177d4ede289
Sophia Thach <sophia.thach@gmail.com>
2015-11-18 09:38:07 -0500

    updated contact to not have a menu

--- index.php
+++ index.php
@@ -26,19 +26,14 @@
         <div class="wrapper">
         <table class="page_table"><tr><td></td><td><center><center>
 		<h1>MedaWiGi</h1>
-		<img src="http://tomshattuck.typepad.com/.a/6a01538f4b955c970b017ee8bf71a8970d-pi" 
-		style="width: 300px; height 500px;">
-
-		<br/>
-
-
+		<img src="./images/clipboard.png" 
+		style="width: 100px; height 200px;">
 		<form id="log" action="checklogin.php" method="POST">
 		Username:<input type="text" name="username" required="required" PLACEHOLDER="Enter your Username"/> <br/>
 		Password: <input type="password" name="password" required="required" PLACEHOLDER="Enter your password"/> <br/>
 
 			<a href="register.php">New User?</a> <br/>
 			<a href="forgotpass.php"> Forgot Password?</a> <br/>
-            <br>
             <br>
 			<input type="submit" value="Login" />
 		</form>
